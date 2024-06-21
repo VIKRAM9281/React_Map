cc46cadfed6774384973c4e857169faa380a392c
VIKRAM9281 <vikrambhosale928@gmail.com>
2024-06-21 16:56:01 +0530

    sm chng

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from '../Subcomponents/Navbar'
 
 const Home = () => {
   return (
     <div>
+       <Routes>
+       <Route path='/' element={<Navbar/>} />
+       </Routes>
       <h4>Home</h4>
     </div>
   )
--- src/Subcomponents/Navbar.jsx
+++ src/Subcomponents/Navbar.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import { pages } from '../Utils/constant'
-
+import React from "react";
+import { pages } from "../Utils/constant";
+import globalcss from "../Css/Global.module.css"
 const Navbar = () => {
-
   return (
     <div>
-    <navbar>
-        <ul>
-{pages.map((item,ind)=>{
-    <li>{item}</li>
-})}
+      <navbar>
+        <ul className={`d-flex justify-content-between ${globalcss.ul}`}>
+          {pages.map((item, ind) => {
+           return <li key={ind}>{item}</li>;
+          })}
         </ul>
-    </navbar>
+      </navbar>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
